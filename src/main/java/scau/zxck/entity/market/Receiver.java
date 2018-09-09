1d0553d376eb6cf89689bf05a5f320ceda42e0bb
201627010201 <1572868725@qq.com>
2018-09-09 14:04:47 +0800

    创建了四个实体类

---
+++ src/main/java/scau/zxck/entity/market/Receiver.java
@@ -0,0 +1,49 @@
+package scau.zxck.entity.market;
+
+public class Receiver {
+    private String id;
+    private String receiver_name;
+    private String deliver_address;
+    private String receiver_cell;
+    private int receiver_sex;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getReceiver_name() {
+        return receiver_name;
+    }
+
+    public void setReceiver_name(String receiver_name) {
+        this.receiver_name = receiver_name;
+    }
+
+    public String getDeliver_address() {
+        return deliver_address;
+    }
+
+    public void setDeliver_address(String deliver_address) {
+        this.deliver_address = deliver_address;
+    }
+
+    public String getReceiver_cell() {
+        return receiver_cell;
+    }
+
+    public void setReceiver_cell(String receiver_cell) {
+        this.receiver_cell = receiver_cell;
+    }
+
+    public int getReceiver_sex() {
+        return receiver_sex;
+    }
+
+    public void setReceiver_sex(int receiver_sex) {
+        this.receiver_sex = receiver_sex;
+    }
+}
